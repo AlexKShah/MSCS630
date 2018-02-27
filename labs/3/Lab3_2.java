8792fae9ff198befae8e295bbbf11f874f060db8
Alexander Shah <AlexKShah@gmail.com>
2018-02-27 15:01:28 -0500

    well it returns 97 a bunch

--- labs/3/Lab3_2.java
+++ labs/3/Lab3_2.java
@@ -9,7 +9,6 @@ public class Lab3_2 {
     Scanner input = new Scanner(System.in);
     char s = input.nextLine().charAt(0);
     String p = input.nextLine();
-
     String newP = "";
     for (int i = 0; i <= p.length(); i++) {
       while (newP.length() < 16) {
@@ -18,18 +17,18 @@ public class Lab3_2 {
         } else if (i >= p.length()) {
           newP += s;
         }
-        System.out.println(getHexMatP(s, newP)); //puts out garbage
-        newP = "";
+        getHexMatP(s, newP);
       }
     }
   }
 
-  static byte[][] getHexMatP(char s, String p) {
-    byte[][] cipher = new byte[4][4];
-    for (int row = 0; row < 4; row++) {
-      for (int col = 0; col < 4; col++) {
-        for (int i = 0; i < p.length(); i++) {
-          cipher[row][col] = (byte) p.charAt(i);
+  static int[][] getHexMatP(char s, String p) {
+    int[][] cipher = new int[4][4];
+    for (int i = 0; i < p.length(); i++) {
+      for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
+          cipher[row][col] = (int) p.charAt(i);
+          System.out.println(cipher[row][col]); //prints "97" a bunch, why?
         }
       }
     }
