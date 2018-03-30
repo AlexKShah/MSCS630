48c72dcda93ed973d52e37126e7a996393504e03
Alexander Shah <AlexKShah@gmail.com>
2018-03-30 17:22:50 -0400

    Start lab 4 template out steps

---
+++ labs/4/AESCipher.java
@@ -0,0 +1,15 @@
+/**
+ * file: AESCipher.java
+ * author: Alex Shah
+ * course: MSCS 630
+ * assignment: Lab 4
+ * due date: 4/4/18
+ * version: 1.0
+ * <p>
+ * templating Lab4 steps
+ */
+
+public class DriverAES {
+  public static void main(String[] args) {
+  }
+}
---
+++ labs/4/DriverAES.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * file: DriverAES.java
+ * author: Alex Shah
+ * course: MSCS 630
+ * assignment: Lab 4
+ * due date: 4/4/18
+ * version: 1.0
+ * <p>
+ * templating Lab4 steps
+ */
+
+public class DriverAES {
+  public static void main(String[] args) {
+    //input: some key
+    Scanner input = new Scanner(System.in);
+    String key = input.next().toUpperCase();
+    //print initial key
+    System.out.println(key);
+    //do stuff to key to get 10 generated keys
+    //System.out.println(AESCipher.AES(key));
+  }
+}
